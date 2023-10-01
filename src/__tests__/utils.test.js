699d74fa6de3f5a4ccdaa3ddf9d7aca328aa8c69
Duke <dukemorise33@gmail.com>
2023-09-30 18:48:06 -0700

    finished tdd unit test exercise

--- src/__tests__/utils.test.js
+++ src/__tests__/utils.test.js
@@ -1,1 +1,12 @@
 // Your tests here
+//I expect a return of 7 for the word 'test'
+import { pointsForWord } from "../utils";
+
+describe('pointsForWord',()=>{
+    it('calculates the total points for a word (1 point per vowel, 2 per consonant)', ()=>{
+        const word = 'test';
+        const wordPoint = pointsForWord(word);
+
+        expect(wordPoint).toBe(7);
+    })
+})
--- src/utils.js
+++ src/utils.js
@@ -1,1 +1,14 @@
 // Your code here
+export function pointsForWord(word){
+    let points = 0;
+    const wordArr = word.split('');
+    wordArr.forEach((letter) => {
+        if(['a','e','i','o','u'].includes(letter)){
+            points ++;
+        }
+        else if(letter){
+            points = points + 2;
+        }
+    })
+    return points;
+}
